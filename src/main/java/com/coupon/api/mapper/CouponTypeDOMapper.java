6fb0d897d8cb628fa2245b2d6623a91fa77c8f15
m13339913887@163.com <lm19900911>
2019-04-16 20:02:59 +0800

    细节调整

--- src/main/java/com/coupon/api/mapper/CouponTypeDOMapper.java
+++ src/main/java/com/coupon/api/mapper/CouponTypeDOMapper.java
@@ -1,7 +1,12 @@
 package com.coupon.api.mapper;
 
+import com.coupon.api.dto.CouponTypeDTO;
 import com.coupon.api.entity.CouponTypeDO;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface CouponTypeDOMapper extends Mapper<CouponTypeDO> {
+    List<CouponTypeDTO> queryList(CouponTypeDO couponTypeDO);
+    int queryCount(CouponTypeDO couponTypeDO);
 }
